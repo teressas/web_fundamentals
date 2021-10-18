24c3bd00f223a74d808342b24dcc159f0b3378ab
Teressa Sung <teressas.hawaii@gmail.com>
2021-10-18 16:13:18 -0700

    initial commit

---
+++ js_assignments/button_clicker/main.js
@@ -0,0 +1,15 @@
+//change the text to say "Logout" when clicking the "Login" button
+function changeLogout(element) {
+    element.innerText = "Logout";
+}
+
+//remove the Add Definition button when it is clicked
+function hide(element) {
+    element.remove();
+}
+
+//alert "Ninja was liked" when clicking a like button
+function example(element){
+    console.log("Ninja was liked", element);
+}
+
