63918fe1ce0a30ec66a091800ed2c1675ae16aab
Teressa Sung <teressas.hawaii@gmail.com>
2021-10-19 14:49:07 -0700

    initial commit

---
+++ js_assignments/liking_likes_part1/script.js
@@ -0,0 +1,11 @@
+var count = 0;
+var button = document.querySelector("#button");
+
+// console.log(countElement)
+
+function add1(){
+    count++;
+    //changes the text in the console.log
+    //countElement.innerText = "The count is " + count;
+    console.log(count);
+}
