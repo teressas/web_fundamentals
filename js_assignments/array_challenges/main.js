05a26516139ee4c6dbc4af957b8e283ade6f7757
Teressa Sung <teressas.hawaii@gmail.com>
2021-10-22 21:15:52 -0700

    commit2

--- js_assignments/array_challenges/main.js
+++ js_assignments/array_challenges/main.js
@@ -5,7 +5,7 @@
 
 function alwaysHungry(arr) {
     for(var i=0; i < arr.length; i++){
-        if (i = "food"){
+        if (arr[i] == "food"){
             console.log("yummy","yummy");
         } else {
             console.log("I'm hungry");
